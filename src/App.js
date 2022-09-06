7e57c9b63f241e198e552d91d89bff8ee50ee9c5
05August <thuanthieu90@gmailcom>
2022-09-06 21:30:27 +0700

    update header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 // import TodoItem from './component/TodoItem.jsx';
-import TodoItemVer2 from './component/TodoItemVer2.jsx';
+// import TodoItemVer2 from './component/TodoItemVer2.jsx';
+// import NewComponent from './component/NewComponent.jsx';
+import Header from './component/header/Header.jsx';
 import './App.css';
 
 function App() {
   return (
-    // <TodoItem/>
-    // <TodoItemVer2/>,
-    <TodoItemVer2/>
+    
+    <div>
+      {/* <NewComponent/>
+      <TodoItem/>
+      <TodoItemVer2/> */}
+      <Header/>
+    </div>
   );
 }
 
---
+++ src/component/header/Header.jsx
@@ -0,0 +1,14 @@
+import '../../style/styleHeader.css'
+const Header=()=>{
+  return <div className="header">
+    <div className="new-task">
+      <div className="btn-new-task">Create New Task</div>
+    </div>
+    <div className="search-task">
+      <input type="text" placeholder="Type something to search" name="searchInput" id="search-input"/>
+      <label for="searchInput">Search</label>
+    </div>
+  </div>
+}
+
+export default Header;
