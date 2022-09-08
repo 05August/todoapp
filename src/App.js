4fd1cf844307046c72158c39e9d25083cea0d24e
05August <thuanthieu90@gmailcom>
2022-09-08 20:11:45 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-// import TodoItem from './component/TodoItem.jsx';
-// import TodoItemVer2 from './component/TodoItemVer2.jsx';
-// import NewComponent from './component/NewComponent.jsx';
-import Header from './component/header/Header.jsx';
-=======
 import TodoItem from './component/TodoItem.jsx';
 import Header from './component/Header.jsx';
->>>>>>> Stashed changes
-=======
-import TodoItem from './component/TodoItem.jsx';
-// import TodoItemVer2 from './component/TodoItemVer2.jsx';
-// import NewComponent from './component/NewComponent.jsx';
-import Header from './component/Header.jsx';
->>>>>>> pratice
 import './App.css';
 import './styles/style.css';
 
