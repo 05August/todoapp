d3ce34e97f34ef1f099c40c61275dda6f1573017
05August <thuanthieu90@gmailcom>
2022-09-20 19:38:03 +0700

    update ui

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import Header from './layout/Header.jsx';
 import Sidebar from './layout/Sidebar.jsx';
 import Body from './layout/Body'
 import Footer from './layout/Footer.jsx';
-import Example from './component/Example.jsx'
 import './App.css';
 import './styles/style.css';
 
 function App() {
   return (
-    // <div className="layout">
-    //   <Header/>
-    //   <Sidebar/>
-    //   <Body/>
-    //   <Footer/>
-    // </div>
-
-    <div>
-      <Example/>
+    <div className="layout">
+      <Header/>
+      <Sidebar/>
+      <Body/>
+      <Footer/>
     </div>
   );
 }
